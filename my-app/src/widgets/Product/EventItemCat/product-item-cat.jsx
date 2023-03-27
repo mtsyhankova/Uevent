027e76a11857037e6020e6f758ca8a57f38a51d2
Inna <eviefryeog@gmail.com>
2023-03-27 23:47:24 +0300

    пагинация карточек

--- my-app/src/widgets/Product/EventItemCat/product-item-cat.jsx
+++ my-app/src/widgets/Product/EventItemCat/product-item-cat.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import luffy from './assets/luffy.jpg'
-
+import Pagination from '../Pagination/pagination';
 
 import "./style.css"
 
 export const ProductItemsCat = () => {
+    const [page, setPage] = useState([0]);
 
     const eventItems = [{ date: "12.04.2023", name: "Абобус", location: "Київ", price: "200" },
     { date: "15.04.2023", name: "Абобус", location: "Чернівці", price: "100" },
     { date: "25.04.2023", name: "Абобіще", location: "Харків", price: "150" },
+    { date: "02.05.2023", name: "Два абобуси", location: "Львів", price: "300" },
+    { date: "17.05.2023", name: "Абобіще", location: "Луцьк", price: "120" },
+    { date: "02.05.2023", name: "Два абобуси", location: "Львів", price: "300" },
+    { date: "02.05.2023", name: "Два абобуси", location: "Львів", price: "300" },
+    { date: "17.05.2023", name: "Абобіще", location: "Луцьк", price: "120" },
+    { date: "02.05.2023", name: "Два абобуси", location: "Львів", price: "300" },
+    { date: "02.05.2023", name: "Два абобуси", location: "Львів", price: "300" },
+    { date: "17.05.2023", name: "Абобіще", location: "Луцьк", price: "120" },
+    { date: "02.05.2023", name: "Два абобуси", location: "Львів", price: "300" },
+    { date: "02.05.2023", name: "Два абобуси", location: "Львів", price: "300" },
+    { date: "17.05.2023", name: "Абобіще", location: "Луцьк", price: "120" },
+    { date: "02.05.2023", name: "Два абобуси", location: "Львів", price: "300" },
     { date: "02.05.2023", name: "Два абобуси", location: "Львів", price: "300" },
     { date: "17.05.2023", name: "Абобіще", location: "Луцьк", price: "120" },
     { date: "02.05.2023", name: "Два абобуси", location: "Львів", price: "300" },
@@ -17,7 +30,7 @@ export const ProductItemsCat = () => {
     return (
         < div className='box_one_category_product_c' >
             {
-                eventItems.map(item =>
+                eventItems.slice(page * 4, page * 4 + 4).map(item =>
                     <div>
                         <div className='card-white'>
                             <img className='eventimg' src={luffy} alt="aboba" />
@@ -35,6 +48,7 @@ export const ProductItemsCat = () => {
                     </div>
                 )
             }
+            <Pagination quantity={eventItems.length} numberForPage={4} current={page} change={setPage} />
 
         </div >
     )
