46b6b53ada08e044db83d273da85447ed1b40e5d
Inna <eviefryeog@gmail.com>
2023-04-01 17:23:59 +0300

    цвета

--- my-app/src/widgets/Product/EventItemCat/product-item-cat.jsx
+++ my-app/src/widgets/Product/EventItemCat/product-item-cat.jsx
@@ -28,28 +28,29 @@ export const ProductItemsCat = () => {
     { date: "02.05.2023", name: "Два абобуси", location: "Львів", price: "300" }]
 
     return (
-        < div className='box_one_category_product_c' >
-            {
-                eventItems.slice(page * 4, page * 4 + 4).map(item =>
-                    <div>
-                        <div className='card-white'>
-                            <img className='eventimg' src={luffy} alt="aboba" />
-                            <p className='event-date-text'>{item.date}</p>
-                            <p className='event-name-text'>{item.name}</p>
-                            <p className='event-location-text'>{item.location}</p>
-                            <p className='event-price-text'>{item.price}</p>
-                            <div className='card-pink'>
-                                <p className='event-reverse-date'>{item.date}</p>
-                                <p className='event-reverse-location'>{item.location}</p>
-                                <p className='event-reverse-name'>{item.name}</p>
-                                <span className='event-button'>Придбати</span>
+        <div>
+            <div className='box_one_category_product_c'>
+                {
+                    eventItems.slice(page * 4, page * 4 + 4).map(item =>
+                        <div>
+                            <div className='card-white'>
+                                <img className='eventimg' src={luffy} alt="aboba" />
+                                <p className='event-date-text'>{item.date}</p>
+                                <p className='event-name-text'>{item.name}</p>
+                                <p className='event-location-text'>{item.location}</p>
+                                <p className='event-price-text'>{item.price}</p>
+                                <div className='card-pink'>
+                                    <p className='event-reverse-date'>{item.date}</p>
+                                    <p className='event-reverse-location'>{item.location}</p>
+                                    <p className='event-reverse-name'>{item.name}</p>
+                                    <span className='event-button'>Придбати</span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                )
-            }
+                    )
+                }
+            </div>
             <Pagination quantity={eventItems.length} numberForPage={4} current={page} change={setPage} />
-
-        </div >
+        </div>
     )
 }
