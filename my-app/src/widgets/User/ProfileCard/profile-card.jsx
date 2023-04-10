6027e0d54107341928e5fd8be7acb217bdabbe01
Marelet <vfifwsufyrjdf0709@gmail.com>
2023-04-11 00:44:51 +0300

    profile

--- my-app/src/widgets/User/ProfileCard/profile-card.jsx
+++ my-app/src/widgets/User/ProfileCard/profile-card.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, Link } from 'react-router-dom';
 
 import luffy from './assets/luffy.jpg';
 
@@ -12,7 +12,10 @@ export const ProfileCard = () => {
     const [eventsPage, setEventsPage] = useState([0]);
     let navigate = useNavigate();
 
-    const subsNames = [{ name: 'abobus1' }, { name: 'abobus1' }, { name: 'abobus1' }, { name: 'abobus1' }, { name: 'abobus2' }, { name: 'abobus2' }, { name: 'abobus2' }, { name: 'abobus2' }, { name: 'abobus2' }, { name: 'abobus3' }, { name: 'abobus3' }, { name: 'abobus3' }, { name: 'abobus3' }]
+    const countMassege = 100;// количство непрочитаніх сообщ
+
+    const userData = { name: 'abobus1', status: 'dwdwddwdvv f ugfuegfe fugfug fu' }
+
     const organizersEvents = [{ date: "12.04.2023", name: "Абобус", location: "Місце проведення", city: "Київ", price: "200" },
     { date: "15.04.2023 - 18.04.2023", name: "Абобус", location: "Місце проведення", city: "Київ", price: "100" },
     { date: "25.04.2023", name: "Абобіще", location: "Місце проведення", city: "Київ", price: "150" },
@@ -34,26 +37,18 @@ export const ProfileCard = () => {
 
     return (<div>
         <div className='profile_card_box'>
-            <div>  <img className='profile_img' src={luffy} alt="aboba" /></div>
+            <div>  <img className='profile_page_img' src={luffy} alt="aboba" /></div>
             <div className='event_opened_name_box'>
-                <p className='event_opened_name'>bjhabjhbfjhdbfjb здрасьте</p>
+                <p className='event_opened_name'>{userData.name}</p>
+                <p className='event_opened_name'>{userData.status}</p>
             </div>
-
-            {/* <div className='event_opened_flex_row'>
-                <img className='event_opened_img' src={luffy} alt="aboba" />
-
-                <div className='event_opened_flex_col'>
-                    <div className='event_opened_flex_row'>
-                        <p className='event_opened_date'>{date}</p>
-                        <p className='event_opened_date'>{location}, {city}</p>
-                    </div>
-
-                    <span className='event_opened_button' onMouseEnter={onhover} onMouseLeave={offhover}>{buttonText}</span>
-
-                    <p className='event_opened_description'>Опис події</p>
-                    <p className='event_opened_date'> бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла бла</p>
+            <Link className='message_link' to='#'>
+                {/* <img className='message_img' src={luffy} alt="aboba" /> */}
+                <div className='count_message_block'>
+                    {countMassege > 99 ? <p>99+</p> : <p>{countMassege}</p>}
                 </div>
-            </div> */}
+
+            </Link>
         </div>
 
 
